b0ba975dcc3032580c4f40642ae05f1731616b5c
devicii <asroq98@gmail.com>
2021-09-16 15:36:33 +0900

    update: c02 ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,12 +6,51 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 12:01:48 by hyunjung          #+#    #+#             */
-/*   Updated: 2021/09/16 12:10:00 by hyunjung         ###   ########.fr       */
+/*   Updated: 2021/09/16 15:35:05 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strcapitalize(char *str)
+#include <stdio.h>
+#include <unistd.h>
+
+char	trans_to_low(char *str)
 {
-	//2번째 줄을 보면 패턴이 특수문자 뒤에 대문자로 처리된다.
-	//그것을 활용해 보자.
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (*str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	sc;
+
+	sc = 1;
+	i = 0;
+	trans_to_low(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (sc == 1)
+				str[i] = str[i] - 32;
+			sc = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			sc = 0;
+		else
+			sc = 1;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
