78798d8af9fb1ee9ff4cc0907b4636b00ba55663
Hyunseop Jung <hyunjung@c2r1s4.42seoul.kr>
2021-09-20 09:44:19 +0900

    update: fix ex02

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -6,28 +6,20 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 21:49:19 by hyunjung          #+#    #+#             */
-/*   Updated: 2021/09/15 23:46:08 by hyunjung         ###   ########.fr       */
+/*   Updated: 2021/09/20 09:43:14 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_str_is_alpha(char *str)
-{
+{	
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			return (1);
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			return (1);
-		}
-		else
-		{
+		if (!(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z'))
 			return (0);
-		}		
+		i++;
 	}
+	return (1);
 }
