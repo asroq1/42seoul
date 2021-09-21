fb9c1eb8cf25fb7adea50d833d30b73e2b3b92b1
devicii <asroq98@gmail.com>
2021-09-21 21:45:47 +0900

    update : done c04

--- c04/ex04/ft_putnbt_base.c
+++ c04/ex04/ft_putnbt_base.c
@@ -6,10 +6,16 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 00:30:12 by hyunjung          #+#    #+#             */
-/*   Updated: 2021/09/21 00:42:00 by hyunjung         ###   ########.fr       */
+/*   Updated: 2021/09/21 21:44:14 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_puthcar(char c)
+{
+	write(1, &c, 1);
+}
 
 int	ft_strlen(char *str)
 {
@@ -21,23 +27,63 @@ int	ft_strlen(char *str)
 		str++;
 		cnt++;
 	}
-	return ('cnt');
+	return (cnt);
 }
 
-void	is_this_base(char *str)
+int	is_this_duplicate(char *base)
 {
-	int i;
-	int str_length;
+	int	i;
+	int	j;
 
 	i = 0;
-	str_length = ft_strlen(str);
-	while (str[i] != '\0' str_length > 1)
+	while (base[i] != '\0')
 	{
-		
+		j = i + 1;
+		if (base[i] == '+' || base[i] == '-')
+		{
+			return (1);
+		}
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+			{
+				return (1);
+			}
+			j++;
+		}
+		i++;
 	}
-	
+	return (0);
 }
+
+void	ft_putnbr(long nbr, int base_length, char *base_arr)
+{
+	if (nbr < base_length && nbr >= 0)
+	{	
+		ft_puthcar(base_arr[nbr % base_length]);
+	}
+	else if (nbr < 0 && nbr != -2147483648)
+	{
+		ft_puthcar('-');
+		ft_putnbr(nbr * (-1), base_length, base_arr);
+	}
+	else
+	{
+		ft_putnbr(nbr / base_length, base_length, base_arr);
+		ft_putnbr(nbr % base_length, base_length, base_arr);
+	}
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
+	int	base_length;
 
+	base_length = ft_strlen(base);
+	if (ft_strlen(base) == 0 || ft_strlen(base) == 1 || is_this_duplicate(base))
+	{
+		return ;
+	}
+	base_length = ft_strlen(base);
+	ft_putnbr((long)nbr, base_length, base);
+	return ;
 }
