07eb11dbf33b4c1f842d042a45b5c322ce294e45
devicii <asroq98@gmail.com>
2021-09-25 10:06:53 +0900

    update: c07 files

---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 23:43:11 by hyunjung          #+#    #+#             */
+/*   Updated: 2021/09/24 23:56:09 by hyunjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int *ft_range(int min, int max)
+{
+	int new_arr;
+	int size;
+	int i;
+
+	i = 0;
+	size = (max - min) + 1;
+	new_arr = malloc(sizeof(int) * size);
+	while (1)
+	{
+		new_arr[i] = 
+	}
+	
+}
