92e8932e12c4b4d181ed10293c2dc46d5d84d368
Hyunseop Jung <hyunjung@c7r6s2.42seoul.kr>
2021-11-16 19:18:00 +0900

    uadd : ft functions

--- cadet/libft/ft_bzero.c
+++ cadet/libft/ft_bzero.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/16 18:55:57 by hyunjung          #+#    #+#             */
-/*   Updated: 2021/11/16 19:11:21 by hyunjung         ###   ########.fr       */
+/*   Created: 2021/11/16 18:50:19 by hyunjung          #+#    #+#             */
+/*   Updated: 2021/11/16 19:15:18 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*memset(void *b, int c, size_t len)
+void	bzero(void *s, size_t n)
 {
 	size_t			idx;
-	unsigned char	*mem;
+	unsigned char	*str;
 
-	idx = 0;
-	mem = (unsigned char *)b;
-	while (len != 0)
+	str = (unsigned char *)s;
+	while (n != 0)
 	{
-		mem[idx] = c;
+		str[idx] = 0;
 		idx++;
-		len--;
+		n--;
 	}
 }
