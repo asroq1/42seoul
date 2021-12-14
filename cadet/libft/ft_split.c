d413d388fdf4f483fc14e2ff22e818b983a017e1
Hyunseop Jung <asroq98@gmail.com>
2021-12-14 17:34:22 +0900

    don split

--- cadet/libft/ft_split.c
+++ cadet/libft/ft_split.c
@@ -6,110 +6,78 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:29:05 by hyunjung          #+#    #+#             */
-/*   Updated: 2021/12/13 19:45:26 by hyunjung         ###   ########.fr       */
+/*   Updated: 2021/12/14 17:31:58 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	get_numbers(const char *str, char c)
-{
-	size_t	i;
-	size_t	numbers;
-	size_t	length;
-
-	i = 0;
-	numbers = 0;
-	length = ft_strlen(str);
-	while (i < length)
-	{
-		if (str[i] == c)
-		{
-			numbers++;
-		}
-		i++;
-	}
-	numbers += 1;
-	return (numbers);
-}
-
-static	void	*get_free(char **mem, size_t cnt)
+static char	**ft_free(char **str)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < cnt)
+	while (str[i] != 0)
 	{
-		free(mem[i]);
+		free(str[i]);
 		i++;
 	}
-	free(mem);
+	free(str[i]);
 	return (0);
 }
 
-void	ft_strcpy(char *dest, char const *src, int start, int last)
-{
-	size_t	i;
-
-	i = 0;
-	while (start < last)
-	{
-		dest[i] = src[i];
-		i++;
-		start++;
-	}
-	dest[i] = 0;
-}
-
-static void	get_result(char const *str, char **new_str, char c)
+static char	**ft_seperator(char **new_str, char const *s, char c, size_t count)
 {
 	size_t	i;
 	size_t	j;
-	size_t	start;
+	size_t	next;
 
 	i = 0;
 	j = 0;
-	while (str[i] != 0)
+	while (s[i] != 0 && j < count)
 	{
-		if (str[i] != 0 && str[i] != c)
+		if (s[i] != c)
 		{
-			start = i;
-			while (str[i] != 0 && str[i] != c)
-				i++;
-			(new_str[j] = malloc(sizeof(char) * (i - start + 1)));
-			if (new_str[j] == 0)
-			{
-				get_free(new_str, j);
-				return ;
-			}
-			ft_strcpy(new_str[j], str, start, i);
+			next = i + 1;
+			while (s[next] != 0 && s[next] != c)
+				next++;
+			new_str[j] = ft_substr(s, i, next - i);
+			if (!new_str[j])
+				return (ft_free(new_str));
 			j++;
+			i = next;
 		}
-		else if (str[i] != 0)
+		else
 			i++;
 	}
+	new_str[j] = 0;
+	return (new_str);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**new_str;
-	size_t	n;
+	size_t	word_count;
+	size_t	i;
 
 	if (s == 0)
+		return (NULL);
+	i = 0;
+	word_count = 0;
+	while (s[i] != 0)
 	{
-		return (0);
+		if (s[i] != c)
+		{
+			word_count++;
+			while ((s[i] != c) && s[i] != 0)
+				i++;
+		}
+		else
+			i++;
 	}
-	n = get_numbers(s, c);
-	new_str = malloc(sizeof(char *) * (n + 1));
+	new_str = malloc(sizeof(char *) * word_count + 1);
 	if (new_str == 0)
-	{
 		return (0);
-	}
-	new_str[n] = 0;
-	if (n == 0)
-	{
-		return (new_str);
-	}
-	get_result(s, new_str, c);
+	ft_seperator(new_str, s, c, word_count);
 	return (new_str);
 }
