1aa917a20da359a7315c8b8cb0e6f00f39dc1f56
Hyunseop Jung <asroq98@gmail.com>
2022-02-14 18:11:20 +0900

    add : first commit

--- cadet/ft_print/ft_printf.c
+++ cadet/ft_print/ft_printf.c
@@ -6,11 +6,51 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:14:38 by hyunjung          #+#    #+#             */
-/*   Updated: 2022/01/27 13:42:46 by hyunjung         ###   ########.fr       */
+/*   Updated: 2022/02/14 18:10:33 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
-{
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// int	ft_str_reader()
+// {
 	
+// }
+
+int	ft_printf(const char *str, ...)
+{
+	int		str_length;
+	va_list	ap;
+
+	str_length = 0;
+	va_start(ap, str);
+	while (*str != 0)
+	{
+		if (*str == '%')
+		{
+			printf("!");
+			str++;
+		}
+		else
+		{
+			write(1, str++, 1);
+			printf("str_length = %d\n", str_length);
+			str_length++;
+		}
+	}
+	va_end(ap);
+	return (str_length);
+}
+
+int	main(void)
+{
+	int	test;
+
+	test = ft_printf("1\n");
+
+	// ft_printf("1");
+	printf("res => %d\n", test);
+	 return (0);
 }
