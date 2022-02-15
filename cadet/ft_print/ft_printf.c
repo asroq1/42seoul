5729ffc1b52074c865b9f6ab9a1e02d24593f258
Hyunseop Jung <asroq98@gmail.com>
2022-02-15 15:06:54 +0900

    update : making main logic & fix error

--- cadet/ft_print/ft_printf.c
+++ cadet/ft_print/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:14:38 by hyunjung          #+#    #+#             */
-/*   Updated: 2022/02/14 18:10:33 by hyunjung         ###   ########.fr       */
+/*   Updated: 2022/02/15 15:06:26 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// int	ft_str_reader()
-// {
-	
-// }
+int	ft_str_reader(char c, va_list ap)
+{
+	if (c == 'c')
+	{
+		//함수로 쪼개서 만들기.
+		char	a;
+		a = va_arg(ap, int);
+		write(1, &a, 1);
+	}
+	// 이후 다른 조건문도 추가
+	return (1);
+}
 
 int	ft_printf(const char *str, ...)
 {
@@ -25,18 +33,17 @@ int	ft_printf(const char *str, ...)
 	va_list	ap;
 
 	str_length = 0;
-	va_start(ap, str);
+	va_start(ap, (int)str);
 	while (*str != 0)
 	{
 		if (*str == '%')
 		{
-			printf("!");
+			ft_str_reader(*(++str), ap);
 			str++;
 		}
 		else
 		{
 			write(1, str++, 1);
-			printf("str_length = %d\n", str_length);
 			str_length++;
 		}
 	}
@@ -46,11 +53,6 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	int	test;
-
-	test = ft_printf("1\n");
-
-	// ft_printf("1");
-	printf("res => %d\n", test);
+	ft_printf("my grade is %c", 'A');
 	 return (0);
 }
