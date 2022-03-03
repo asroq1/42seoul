d9264f883129591b975c62029c4693393528c160
asroq1 <asroq98@gmail.com>
2022-03-03 11:59:13 +0900

    delete : dummy if

--- cadet/ft_printf/ft_printf_str.c
+++ cadet/ft_printf/ft_printf_str.c
@@ -6,7 +6,7 @@
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 15:57:07 by hyunjung          #+#    #+#             */
-/*   Updated: 2022/03/03 11:50:19 by hyunjung         ###   ########.fr       */
+/*   Updated: 2022/03/03 11:58:22 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,25 +41,17 @@ int	ft_print_str(const char **str, va_list ap)
 
 int	ft_print_perc(const char **str)
 {
-	if (**str == '%')
-	{
-		write(1, "%", 1);
-		(*str)++;
-		return (1);
-	}
-	return (0);
+	write(1, "%", 1);
+	(*str)++;
+	return (1);
 }
 
 int	ft_printf_char(const char **str, va_list ap)
 {
 	int	i;
 
-	if (**str == 'c')
-	{
-		i = va_arg(ap, int);
-		write(1, &i, 1);
-		(*str)++;
-		return (1);
-	}
-	return (0);
+	i = va_arg(ap, int);
+	write(1, &i, 1);
+	(*str)++;
+	return (1);
 }
