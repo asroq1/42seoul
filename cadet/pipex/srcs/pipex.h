150f5afa72de36a854949ee6d854e8a7137bbcec
asroq1 <asroq98@gmail.com>
2022-03-16 19:27:27 +0900

    update : main function

--- cadet/pipex/srcs/pipex.h
+++ cadet/pipex/srcs/pipex.h
@@ -1,50 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 17:17:32 by hyunjung          #+#    #+#             */
-/*   Updated: 2022/03/16 19:26:59 by hyunjung         ###   ########.fr       */
+/*   Created: 2022/03/15 17:19:05 by hyunjung          #+#    #+#             */
+/*   Updated: 2022/03/16 19:17:54 by hyunjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#ifndef PIPEX_H
+# define PIPEX_H
 
-int parentProc()
-{
+# include <unistd.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <sys/wait.h>
 
-}
-
-int parentProc()
-{
-
-}
-int	main(int argc, char **argv, char **envp)
-{
-	pid_t	pid;
-	int		fd[2];
-
-	proc = fork();
-	pid = pipe(pid);
-	if (argc == 5)
-	{
-		if (pid == -1)
-			errorMessage();
-		pid = pork();
-		if (pid == -1)
-			errorMessage();
-		if (pid == 1)
-		{
-			childProc();
-		}
-		waitpid();
-		parentProc();
-	}
-	else
-	{
-
-	}
-	return (0);
-}
+#endif
