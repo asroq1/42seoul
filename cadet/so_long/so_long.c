c72b7aef5e9bd8dd2ac1ee0d7ecaef53ed22fbd3
asroq1 <asroq98@gmail.com>
2022-04-13 17:09:00 +0900

    add : try to execute mlx library

---
+++ cadet/so_long/so_long.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjung <hyunjung@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 15:34:54 by hyunjung          #+#    #+#             */
+/*   Updated: 2022/04/13 17:06:45 by hyunjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hello");
+	mlx_loop(mlx_ptr);
+}
