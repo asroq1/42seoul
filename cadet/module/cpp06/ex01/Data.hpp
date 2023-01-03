371d90e1654af76bfa13d53ac0f0be59219eb939
asroq1 <asroq98@gmail.com>
2023-01-03 17:51:00 +0900

    update: fix header

--- cadet/module/cpp06/ex01/Data.hpp
+++ cadet/module/cpp06/ex01/Data.hpp
@@ -10,7 +10,4 @@ typedef struct Data {
 
 } Data;
 
-uintptr_t serialize(Data *ptr) { return reinterpret_cast<uintptr_t>(ptr); }
-Data *deserialize(uintptr_t raw) { return reinterpret_cast<Data *>(raw); }
-
 #endif
