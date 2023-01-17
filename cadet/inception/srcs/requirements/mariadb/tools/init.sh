6930461b4ebae2a4621bda8d02a00cfbde96cfc0
asroq1 <asroq98@gmail.com>
2023-01-17 17:34:50 +0900

    update : wordexpress sources

--- cadet/inception/srcs/requirements/mariadb/tools/init.sh
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress_db;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
-# DB_USER에게 모든 권한을 줌
-GRANT ALL PRIVILEGES ON wordpress_db.* TO '$DB_USER'@'%';
-# 위의 db 변경사항을 즉시 반영하도록 하게 한다
-FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';
