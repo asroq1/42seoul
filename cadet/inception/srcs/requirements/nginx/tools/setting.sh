fdeebaae25b49c3aa5c14fe025ddcb2a77a0aaf1
asroq1 <asroq98@gmail.com>
2023-01-27 15:47:11 +0900

    fix : nginx error

--- cadet/inception/srcs/requirements/nginx/tools/setting.sh
+++ cadet/inception/srcs/requirements/nginx/tools/setting.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server_pkey.pem -out /etc/ssl/certs/server.crt
+mkdir /etc/nginx/certificate
+cd /etc/nginx/certificate
 
-# exec "$@"
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key -subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=hyunjung.42.fr";
 
-nginx -g 'daemon off;'
-
+exec "$@"
--- cadet/inception/srcs/requirements/wordpress/tools/setting.sh
+++ cadet/inception/srcs/requirements/wordpress/tools/setting.sh
@@ -14,13 +14,15 @@ echo " __        __            _ ____
 chown -R www-data:www-data /var/www/
 
 
-if [ ! -f "/var/www/html/wordpress/index.php" ]; then # index.php가 존재하지 않을 때 => wp 설치 및 초기화가 안 되어 있을 때만 새로 설치
+if [ ! -f "/var/www/html/index.php" ]; then # index.php가 존재하지 않을 때 => wp 설치 및 초기화가 안 되어 있을 때만 새로 설치
 	sudo -u www-data sh -c " \
     wp core download --locale=en_US && \
     wp config create --dbname=$MYSQL_DB --dbhost=$MYSQL_HOST --dbuser=$MYSQL_USER --dbpass=$MYSQL_PW && \
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_EMAIL && \
     wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS
 	"
+    mv /var/www/html/wordpress/* /var/www/html  
 fi
+
 
 exec /usr/sbin/php-fpm7.3 -F 
