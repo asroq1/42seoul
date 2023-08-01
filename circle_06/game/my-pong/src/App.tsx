ce99d2e0477d8ee2d2a09a8cf914d4bebe130260
Devicii <62472550+asroq1@users.noreply.github.com>
2023-08-01 18:35:26 +0900

    update : typescript ping pong ver

---
+++ circle_06/game/my-pong/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PingPong from './PingPong'
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Hello pong</h2>
+      <PingPong />
+    </div>
+  )
+}
+
+export default App
---
+++ circle_06/game/my-pong/src/PingPong.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PingPong = () => {
+  return (
+    <>
+      <canvas id='pong' width='600' height='400'></canvas>
+    </>
+  )
+}
+
+export default PingPong
